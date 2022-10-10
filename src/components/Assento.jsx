87742568c6e215e3c928d590c770705644f587d4
Daniel Abdel <danielmalek101@gmail.com>
2022-10-10 17:53:14 -0300

    terceira tela em andament2o

--- src/components/Assento.jsx
+++ src/components/Assento.jsx
@@ -9,10 +9,10 @@ import Header from "./Header";
 export default function () {
   const { idSessao } = useParams();
   const [seats, setSeats] = useState([]);
-  const [session,setSession] = useState([]);
+  const [session, setSession] = useState([]);
   const [movieTime, setMovieTime] = useState({});
-  const [time, setTime] = useState()
-  
+  const [time, setTime] = useState();
+
   useEffect(() => {
     const URL = `https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`;
 
@@ -20,10 +20,9 @@ export default function () {
 
     promise.then((resp) => {
       setSeats(resp.data.seats);
-      setSession(resp.data.movie)
-      setMovieTime(resp.data)
-      setTime(resp.data.day.weekday)
-      
+      setSession(resp.data.movie);
+      setMovieTime(resp.data);
+      setTime(resp.data.day.weekday);
     });
 
     promise.catch((error) => {
@@ -36,14 +35,26 @@ export default function () {
       <Header text={"Selecione os assentos"} />
 
       <SeatsContainer>
-        {seats.map((value)=> <MovieSeats>{value.name}</MovieSeats>)}
+        {seats.map((value) => (
+          <MovieSeats key={value.name}>{value.name}</MovieSeats>
+        ))}
       </SeatsContainer>
-
-        <form>
+      <FormContainer>
         
-        </form>  
+          <label for="campo nome">Nome do comprador:</label>
+          <input type="text" placeholder="Digite seu nome" id="campo nome" />
+          <label for="campo cpf">CPF do comprador:</label>
+          <input type="text" placeholder="Digite seu cpf" id="campo cpf" />
+          <button>Reservar assentos</button>
+        
+      </FormContainer>
 
-      <Footer posterURL={session.posterURL} title={session.title} weekday={time} name={movieTime.name}  />
+      <Footer
+        posterURL={session.posterURL}
+        title={session.title}
+        weekday={time}
+        name={movieTime.name}
+      />
     </>
   );
 }
@@ -67,3 +78,27 @@ const MovieSeats = styled.div`
     margin: 7px;
     cursor: pointer;
 `
+const FormContainer = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 20px;
+
+  input {
+    margin: 12px 0;
+  }
+  button {
+    width: 220px;
+    height: 44px;
+    background-color: #e8833a;
+    color: white;
+    font-size: 20px;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+  }
+  button:active {
+    background-color: #b65104;
+    translate: -1px;
+  }
+`;
